533246a2ddef11e35c5d702011cee560cff41c6e
ruisantos <ruisantos@ymail.com>
2023-03-14 13:36:33 +0000

    Documentations for the Models

--- RuiSantos.ZocDoc.Core/Resources/DependenciesExtensions.cs
+++ RuiSantos.ZocDoc.Core/Resources/DependenciesExtensions.cs
@@ -4,8 +4,15 @@ using RuiSantos.ZocDoc.Core.Managers;
 
 namespace RuiSantos.ZocDoc.Core;
 
+/// <summary>
+/// Extension methods for registering dependencies in the container.
+/// </summary>
 public static class DependenciesExtensions
 {
+    /// <summary>
+    /// Registers the services that are used by the application.
+    /// </summary>
+    /// <param name="container">The container builder.</param>
     public static void RegisterZocDocServices(this ContainerBuilder container) {
         container.RegisterType<DomainContext>().As<IDomainContext>().SingleInstance();
 
